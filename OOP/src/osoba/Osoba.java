10cafc98b611a5435cae94d31153d2586940123f
Josip-github <josipcota1@gmail.com>
2021-07-05 16:04:53 +0200

    Vjezbanje OOP-a
    SetId - Random
    SetEmail - concatenation

---
+++ OOP/src/osoba/Osoba.java
@@ -0,0 +1,42 @@
+package osoba;
+
+public class Osoba {
+
+	private String ime;
+	private int id;
+	private String email;
+	private String nacionalnost;
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNacionalnost() {
+		return nacionalnost;
+	}
+
+	public void setNacionalnost(String nacionalnost) {
+		this.nacionalnost = nacionalnost;
+	}
+
+}
---
+++ OOP/src/osoba/Start.java
@@ -0,0 +1,52 @@
+package osoba;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Start {
+
+	Random random = new Random();
+	Osoba[] osobe;
+	
+	public Start() {
+		
+		osobe = new Osoba[3];
+		ucitajOsobe();
+		ispisiOsobe();
+	}
+
+	private void ispisiOsobe() {
+
+		for (Osoba osoba : osobe) {
+			System.out.println(osoba.getIme());
+			System.out.println(osoba.getNacionalnost());
+			System.out.println(osoba.getEmail());
+			System.out.println(osoba.getId());
+			System.out.println();
+		}
+	}
+
+	private void ucitajOsobe() {
+
+		Osoba osoba;
+		
+		for(int i = 0; i < osobe.length; i++) {
+			
+			osoba = new Osoba();
+			osoba.setIme(JOptionPane.showInputDialog("Unesite ime osobe"));
+			osoba.setEmail(osoba.getIme() + "firma@gmail.com");
+			osoba.setNacionalnost(
+					JOptionPane.showInputDialog("Unesite nacionalnost osobe"));
+			osoba.setId(random.nextInt());
+			
+			osobe[i] = osoba;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		new Start();
+	}
+}
