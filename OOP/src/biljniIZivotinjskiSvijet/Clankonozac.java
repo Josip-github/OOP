1d0158a6b021ae7a51fda5c1a6a013a465776351
Josip-github <josipcota1@gmail.com>
2021-07-07 10:48:19 +0200

    Clankonozac i Sisavac
    Clankonozac nasljeđuje od klase beskralježnjak, a Sisavac od klase
    Kralježnjak

--- OOP/src/biljniIZivotinjskiSvijet/Clankonozac.java
+++ OOP/src/biljniIZivotinjskiSvijet/Clankonozac.java
@@ -1,5 +1,35 @@
 package biljniIZivotinjskiSvijet;
 
-public class Clankonozac {
+public class Clankonozac extends Beskraljeznjak {
+
+	private boolean presvlacenje;
+	private String gradja;
+
+	public Clankonozac() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Clankonozac(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, boolean pokretljivost2,
+			boolean imaKraljeznicu, String okolis, boolean presvlacenje, String gradja) {
+		super(pokretljivost, razmnozavanje, probavniSustav, pokretljivost2, imaKraljeznicu, okolis);
+		this.presvlacenje = presvlacenje;
+		this.gradja = gradja;
+	}
+
+	public boolean isPresvlacenje() {
+		return presvlacenje;
+	}
+
+	public void setPresvlacenje(boolean presvlacenje) {
+		this.presvlacenje = presvlacenje;
+	}
+
+	public String getGradja() {
+		return gradja;
+	}
+
+	public void setGradja(String gradja) {
+		this.gradja = gradja;
+	}
 
 }
--- OOP/src/biljniIZivotinjskiSvijet/Sisavac.java
+++ OOP/src/biljniIZivotinjskiSvijet/Sisavac.java
@@ -1,5 +1,35 @@
 package biljniIZivotinjskiSvijet;
 
-public class Sisavac {
+public class Sisavac extends Kraljeznjak {
+
+	private boolean zubi;
+	private String hranjenjePodmlatka;
+
+	public Sisavac() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sisavac(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, boolean pokretljivost2,
+			boolean imaKraljeznicu, String okolis, boolean zubi, String hranjenjePodmlatka) {
+		super(pokretljivost, razmnozavanje, probavniSustav, pokretljivost2, imaKraljeznicu, okolis);
+		this.zubi = zubi;
+		this.hranjenjePodmlatka = hranjenjePodmlatka;
+	}
+
+	public boolean isZubi() {
+		return zubi;
+	}
+
+	public void setZubi(boolean zubi) {
+		this.zubi = zubi;
+	}
+
+	public String getHranjenjePodmlatka() {
+		return hranjenjePodmlatka;
+	}
+
+	public void setHranjenjePodmlatka(String hranjenjePodmlatka) {
+		this.hranjenjePodmlatka = hranjenjePodmlatka;
+	}
 
 }
