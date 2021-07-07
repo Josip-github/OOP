df0f31ca2f6cfb660ffdcbffd11ae4ecf721d7ec
Josip-github <josipcota1@gmail.com>
2021-07-07 10:52:02 +0200

    Rak i Deva
    Klasa Rak nasljeđuje od klase Clankonozac, a klasa Deva nasljeđuje od
    klase Sisavac

--- OOP/src/biljniIZivotinjskiSvijet/Deva.java
+++ OOP/src/biljniIZivotinjskiSvijet/Deva.java
@@ -1,5 +1,36 @@
 package biljniIZivotinjskiSvijet;
 
-public class Deva {
+public class Deva extends Sisavac {
+
+	private int velicina;
+	private String boja;
+
+	public Deva() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Deva(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, boolean pokretljivost2,
+			boolean imaKraljeznicu, String okolis, boolean zubi, String hranjenjePodmlatka, int velicina, String boja) {
+		super(pokretljivost, razmnozavanje, probavniSustav, pokretljivost2, imaKraljeznicu, okolis, zubi,
+				hranjenjePodmlatka);
+		this.velicina = velicina;
+		this.boja = boja;
+	}
+
+	public int getVelicina() {
+		return velicina;
+	}
+
+	public void setVelicina(int velicina) {
+		this.velicina = velicina;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
 
 }
--- OOP/src/biljniIZivotinjskiSvijet/Rak.java
+++ OOP/src/biljniIZivotinjskiSvijet/Rak.java
@@ -1,5 +1,36 @@
 package biljniIZivotinjskiSvijet;
 
-public class Rak {
+public class Rak extends Clankonozac {
+
+	private int velicina;
+	private String boja;
+
+	public Rak() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rak(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, boolean pokretljivost2,
+			boolean imaKraljeznicu, String okolis, boolean presvlacenje, String gradja, int velicina, String boja) {
+		super(pokretljivost, razmnozavanje, probavniSustav, pokretljivost2, imaKraljeznicu, okolis, presvlacenje,
+				gradja);
+		this.velicina = velicina;
+		this.boja = boja;
+	}
+
+	public int getVelicina() {
+		return velicina;
+	}
+
+	public void setVelicina(int velicina) {
+		this.velicina = velicina;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
 
 }
