f9fe25a590f8fd6562aa1f40152c821c489ca684
Josip-github <josipcota1@gmail.com>
2021-07-07 10:39:54 +0200

    Mahovina i kaktus
    Mahovina nasljeđuje od klase BiljkeVlaznihStanista, a kaktus od klase
    BiljkeSuhihStanista

--- OOP/src/biljniIZivotinjskiSvijet/Kaktus.java
+++ OOP/src/biljniIZivotinjskiSvijet/Kaktus.java
@@ -1,5 +1,35 @@
 package biljniIZivotinjskiSvijet;
 
-public class Kaktus {
+public class Kaktus extends BiljkeSuhihStanista {
+
+	private int velicina;
+	private String oblik;
+
+	public Kaktus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Kaktus(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, String boja, String okolis,
+			boolean imaLisce, int velicina, String oblik) {
+		super(pokretljivost, razmnozavanje, probavniSustav, boja, okolis, imaLisce);
+		this.velicina = velicina;
+		this.oblik = oblik;
+	}
+
+	public int getVelicina() {
+		return velicina;
+	}
+
+	public void setVelicina(int velicina) {
+		this.velicina = velicina;
+	}
+
+	public String getOblik() {
+		return oblik;
+	}
+
+	public void setOblik(String oblik) {
+		this.oblik = oblik;
+	}
 
 }
--- OOP/src/biljniIZivotinjskiSvijet/Mahovina.java
+++ OOP/src/biljniIZivotinjskiSvijet/Mahovina.java
@@ -1,5 +1,34 @@
 package biljniIZivotinjskiSvijet;
 
-public class Mahovina {
+public class Mahovina extends BiljkeVlaznihStanista {
 
+	private int velicina;
+	private String oblik;
+
+	public Mahovina() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Mahovina(boolean pokretljivost, boolean razmnozavanje, boolean probavniSustav, String boja, String okolis,
+			boolean imaLisce, int velicina, String oblik) {
+		super(pokretljivost, razmnozavanje, probavniSustav, boja, okolis, imaLisce);
+		this.velicina = velicina;
+		this.oblik = oblik;
+	}
+
+	public int getVelicina() {
+		return velicina;
+	}
+
+	public void setVelicina(int velicina) {
+		this.velicina = velicina;
+	}
+
+	public String getOblik() {
+		return oblik;
+	}
+
+	public void setOblik(String oblik) {
+		this.oblik = oblik;
+	}
 }
